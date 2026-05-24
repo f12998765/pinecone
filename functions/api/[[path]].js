aaf7d194a16ea80e45f4eb2cae93cd4990735df6
XIZERO <f12998765@yeah.net>
2026-05-24 13:48:12 +0800

    fix

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,5 +1,7 @@
 export async function onRequest({ request, env }) {
   const url = new URL(request.url);
   url.hostname = env.LINKDING_HOST;
-  return fetch(url.toString(), { headers: request.headers });
+  const headers = new Headers(request.headers);
+  headers.set('Host', env.LINKDING_HOST);
+  return fetch(url.toString(), { headers });
 }
